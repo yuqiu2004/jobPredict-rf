8c68c5f907f82c46700bc509c6730ccb881c5912
yuqiu2004 <2506852507@qq.com>
2024-12-27 20:55:15 +0800

    feat : 利用flask封装到api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src.train import load_and_train
 from src.predict import predict
+from flask import Flask, request, jsonify
 
 data_path = './data/job_simplified.csv'
 model_path = './models/random_forest_model.pkl'
 encoder_path = './models/label_encoder.joblib'
+app = Flask('job-predict-api')
 
 def train_and_save():
     load_and_train(data_path, model_path, encoder_path)
@@ -13,6 +15,35 @@ def load_and_test():
     predict(model_path, encoder_path)
     print(f'=== predict end... ===')
 
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+#Post接口 用于远程调用
+@app.route('/predict', methods=['POST'])
+def predict_api():
+    try:
+        data = request.get_json()
+        # 提取 JSON 数据中的各个字段
+        qualification = data.get("qualification")
+        salary = data.get("salary")
+        company_size = data.get("company_size")
+        preference = data.get("preference")
+        skills = data.get("skills")
+        responsibilities = data.get("responsibilities")
+        # 检查输入数据是否完整
+        if not all([qualification, salary, company_size, preference, skills, responsibilities]):
+            return jsonify({"error": "Missing required fields in the input data."}), 400
+
+        # 将提取的数据放入 user_input 列表
+        user_input_list = [qualification, salary, company_size, preference, skills, responsibilities]
+        result = predict(model_path, encoder_path, user_input_list)
+        print(result)
+        return jsonify({"job": result.tolist()[0]}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == "__main__":
-    load_and_test()
+    app.run(host='0.0.0.0', port=8081, debug=True)
+    # load_and_test()
     # train_and_save()
--- src/predict.py
+++ src/predict.py
@@ -4,14 +4,17 @@ from src.utils.data_solve import handle_input
 
 column_names = ['Qualifications', 'Salary', 'CompanySize', 'Preference', 'Skills', 'Responsibilities']
 
-def predict(path, encoder):
+def predict(path, encoder, params=''):
     # 加载模型和 label_encoder
     rfc = joblib.load(path)
     label_encoder = joblib.load(encoder)
 
     # 将用户输入的数据转化为列表并转换为数字
-    user_input = ['M', '79000', '26000', 'Female', 'Social media platforms (e.g., Facebook, Twitter, Instagram) Content creation and scheduling Social media analytics and insights Community engagement Paid social advertising', 'Manage and grow social media accounts, create engaging content, and interact with the online community. Develop social media content calendars and strategies. Monitor social media trends and engagement metrics.']
-
+    user_input = []
+    if not params:
+        user_input = ['M', '79000', '26000', 'Female', 'Social media platforms (e.g., Facebook, Twitter, Instagram) Content creation and scheduling Social media analytics and insights Community engagement Paid social advertising', 'Manage and grow social media accounts, create engaging content, and interact with the online community. Develop social media content calendars and strategies. Monitor social media trends and engagement metrics.']
+    else:
+        user_input = params
     # 将用户输入转换为 DataFrame，保持列名顺序
     input_data = pd.DataFrame([user_input], columns=column_names)
     # print(input_data)
@@ -21,4 +24,5 @@ def predict(path, encoder):
     # 解码预测结果
     y_pre_text = label_encoder.inverse_transform(y_pre)
     # 输出预测结果
-    print("预测结果：", y_pre_text)
+    # print("预测结果：", y_pre_text)
+    return y_pre_text
